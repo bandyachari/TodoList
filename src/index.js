6322874685b8077ba5ceac83eb5f07377632bd4e
sandhya <sandhya.chari@dwyergroup.com>
2024-04-02 15:19:44 +0530

    added an alt attribute to table

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
-// let age = windows.promt("how old are you");
-
-// age = age+1;
-
-console.log("hello world");
-
-
-
-
-console.log("hello world");
-
 function addRow() {
+
+    const formm = document.getElementById("inputform")
+    const all_tasks = [];
+    formm.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        
+        //         const each_task = document.getElementById("each_task").value;
+        //         const date_picked = document.getElementById("date_picked").value;
+        //         const late = document.getElementById("late").value;
+
+
+        // const each_person = {
+        //     newTask: each_task,
+        //     newDatePicked: date_picked,
+        //     newLate: late
+        // };
+
+
+        // all_tasks.push(each_person);
+
+        // // date_picked = "";
+        
+        // console.log(all_tasks);
+    });
     
-    const form = document.getElementById("inputform")
-    form.addEventListener("click", function(event){
-        event.preventDefault()
-      });
-    
-    const formData = new FormData(form);
+   const formData = new FormData(formm);
+
+    // console.log(formData);
+
+
+
+
 
     let tabl = document.getElementById('tab');
     const row = document.createElement('tr');
@@ -27,10 +42,11 @@ function addRow() {
         data.innerHTML = pair[1]
         row.appendChild(data);
     }
+
+    
     tabl.appendChild(row);
-    var allinputs = document.querySelectorAll('input')
-    allinputs.forEach(singleinput => singleinput.value ='')
-    // let msg = prompt("give your name")
+    //document.getElementById("inputform").reset();
+
     console.log("inside Addrow")
 }
 
